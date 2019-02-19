c87c9dbcdd40b7de9a18a6df78a2a4f93c1e36c4
navyasharma <ashrinavya3@gmail.com>
2019-02-19 10:01:24 +0530

    python file

---
+++ colo.py
@@ -0,0 +1,7 @@
+#a="ram"
+#b="boy"
+#c=23
+#print("%s is a good %s and his age is %d year"%(a,b,c))
+print("%s whoes rollno is %d got %f in the last semster"%("ram",34,6.8))
+print("%s has a rating of %f on %s"%("sham",4.2,"TotalDhmal"))
+print("the value of %s is %.2f"%("IBM",4.72344))
